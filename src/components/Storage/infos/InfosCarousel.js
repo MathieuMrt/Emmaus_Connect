cc49b4d4a0c935e70ca29e8040d1799ec2e9d060
kaezersoze <fingerzjbm@gmail.com>
2023-06-29 10:11:57 +0200

    HKaKa

---
+++ src/components/Storage/infos/InfosCarousel.js
@@ -0,0 +1,82 @@
+import image5 from "../../../Assets/image5.jpg"
+import image6 from "../../../Assets/image6.jpg"
+import image7 from "../../../Assets/image7.jpg"
+import image8 from "../../../Assets/image8.jpg"
+
+const carousel = [
+    {
+        nom:"Iphone 6",
+        img:image5
+    },
+    {
+        nom:" Iphone 10",
+        img:image5
+    },
+    {
+        nom:"Iphone 12",
+        img:image5
+    },
+    {
+        nom:"Huawaii P20",
+        img:image6
+    },
+    {
+        nom:"Huawaii P40",
+        img:image6
+    },
+    {
+        nom:"Samsung A10",
+        img:image7
+    },
+    {
+        nom:"Samsung S22",
+        img:image7
+    },{
+        nom:"Samsung S23",
+        img:image7
+    },
+    {
+        nom:"Google 7Pro",
+        img:image8
+    },
+    {
+        nom:"Google 6A",
+        img:image8
+    },
+    {
+        nom:"Google 4A",
+        img:image8
+    },
+    {
+        nom:"",
+        img:""
+    },
+    {
+        nom:"",
+        img:""
+    },
+    {
+        nom:"",
+        img:""
+    },
+    {
+        nom:"",
+        img:""
+    },
+    {
+        nom:"",
+        img:""
+    },
+    {
+        nom:"",
+        img:""
+    },
+    {
+        nom:"",
+        img:""
+    },
+
+
+]
+
+export default carousel;
